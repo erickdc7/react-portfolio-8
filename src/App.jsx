10f7ee82a9920623174bcf06d8269cf6c85598e9
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-09-12 13:48:36 -0500

    add import component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Hero from './components/hero/Hero'
 import Services from './components/services/Services'
 import Portfolio from './components/portfolio/Portfolio'
+import Contact from './components/contact/Contact'
 
 const App = () => {
   return (
