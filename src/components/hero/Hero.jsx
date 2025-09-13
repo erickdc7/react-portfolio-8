ff5ff8181823ff157df4720237c6f7d8e39333a2
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-09-13 12:12:27 -0500

    add svg

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -24,7 +24,25 @@ const Hero = () => {
 
                 {/* SCROLL SVG */}
                 <a href="#services">
-
+                    <svg
+                        width="50px"
+                        height="50px"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="M5 9C5 5.13401 8.13401 2 12 2C15.866 2 19 5.13401 19 9V15C19 18.866 15.866 22 12 22C8.13401 22 5 18.866 5 15V9Z"
+                            stroke="white"
+                            strokeWidth="1"
+                        />
+                        <path
+                            d="M12 5V8"
+                            stroke="white"
+                            strokeWidth="1"
+                            strokeLinecap="round"
+                        />
+                    </svg>
                 </a>
             </div>
 
