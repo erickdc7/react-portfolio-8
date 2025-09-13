394b0e7741f4d55c1e3bdadd73856c1bffce7c02
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-09-13 11:58:34 -0500

    add h1

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,7 +3,11 @@ import "./hero.css"
 const Hero = () => {
     return (
         <div className="hero">
-            <div className="hSection left"></div>
+            <div className="hSection left">
+                <h1 className="hTitle">
+
+                </h1>
+            </div>
             <div className="hSection right"></div>
         </div>
     )
