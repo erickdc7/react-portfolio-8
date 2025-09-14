583051784037e64e22606ccdaf74b50f71bddc46
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-09-14 13:37:48 -0500

    add text

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -69,6 +69,9 @@ const Hero = () => {
                 {/* CERTIFICATE */}
                 <div className="certificate">
                     <img src="/certificate.png" alt="" />
+                    LMA CERTIFICED<br />
+                    PROFESSIONAL<br />
+                    UI DESIGNER
                 </div>
             </div>
         </div>
