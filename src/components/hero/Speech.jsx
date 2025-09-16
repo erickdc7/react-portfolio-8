5aec168e08a9c992e9c230947c8a20e234e9fb90
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-09-16 12:08:03 -0500

    add code

--- src/components/hero/Speech.jsx
+++ src/components/hero/Speech.jsx
@@ -14,6 +14,7 @@ const Speech = () => {
                     ]}
                     wrapper="span"
                     speed={40}
+                    deletionSpeed={60}
                     repeat={Infinity}
                 />
             </div>
