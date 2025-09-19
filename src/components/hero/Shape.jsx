d7b53c322ed05b9aac22bcf3933454eb9d25a356
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-09-19 13:27:27 -0500

    add meshdistormaterial

--- src/components/hero/Shape.jsx
+++ src/components/hero/Shape.jsx
@@ -1,10 +1,12 @@
-import { Sphere } from "@react-three/drei"
+import { MeshDistortMaterial, Sphere } from "@react-three/drei"
 
 const Shape = () => {
     return (
         <>
             <Sphere args={[1, 100, 200]} scale={2.4}>
+                <MeshDistortMaterial
 
+                />
             </Sphere>
         </>
     )
