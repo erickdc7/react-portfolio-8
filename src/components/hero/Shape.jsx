cd96d9056e952c9ca86ec0833c6c054c8d1fe76e
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-09-19 13:26:26 -0500

    add property

--- src/components/hero/Shape.jsx
+++ src/components/hero/Shape.jsx
@@ -3,7 +3,7 @@ import { Sphere } from "@react-three/drei"
 const Shape = () => {
     return (
         <>
-            <Sphere>
+            <Sphere args={[1, 100, 200]} scale={2.4}>
 
             </Sphere>
         </>
