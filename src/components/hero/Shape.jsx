f5a50799314ed1a504814341a7c3cc5b0374628b
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-09-19 13:28:38 -0500

    add property

--- src/components/hero/Shape.jsx
+++ src/components/hero/Shape.jsx
@@ -5,7 +5,10 @@ const Shape = () => {
         <>
             <Sphere args={[1, 100, 200]} scale={2.4}>
                 <MeshDistortMaterial
-
+                    color="#DB8B9B"
+                    attach="material"
+                    distort={0.5}
+                    speed={2}
                 />
             </Sphere>
         </>
