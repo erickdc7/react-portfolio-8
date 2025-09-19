087341245c29a242941647ceae00d2106ec8ae37
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-09-19 13:24:43 -0500

    delete code

--- src/components/hero/Shape.jsx
+++ src/components/hero/Shape.jsx
@@ -2,10 +2,8 @@ import { Canvas } from "@react-three/fiber"
 
 const Shape = () => {
     return (
-        <Canvas>
-            <mesh>
+        <>
 
-            </mesh>
-        </Canvas>
+        </>
     )
 }
