e531ccc22b318be401ea5053e3826d4aef9f5aed
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-09-20 12:46:38 -0500

    add console model

--- src/components/services/console/ConsoleModelContainer.jsx
+++ src/components/services/console/ConsoleModelContainer.jsx
@@ -1,6 +1,6 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
-import { ComputerModel } from "./ComputerModel";
+import { ComputerModel } from "./ConsoleModel";
 import { OrbitControls, PerspectiveCamera, Stage } from "@react-three/drei";
 
 const ComputerModelContainer = () => {
