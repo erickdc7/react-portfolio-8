543e85ad12fcaae341a0c65b7698965af484debc
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-09-20 12:47:42 -0500

    add console model

--- src/components/services/console/ConsoleModelContainer.jsx
+++ src/components/services/console/ConsoleModelContainer.jsx
@@ -1,6 +1,6 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
-import { ComputerModel } from "./ConsoleModel";
+import { ConsoleModel } from "./ConsoleModel";
 import { OrbitControls, PerspectiveCamera, Stage } from "@react-three/drei";
 
 const ConsoleModelContainer = () => {
@@ -8,7 +8,7 @@ const ConsoleModelContainer = () => {
         <Canvas>
             <Suspense fallback="loading...">
                 <Stage environment="night" intensity={0.5}>
-                    <ComputerModel />
+                    <ConsoleModel />
                 </Stage>
                 <OrbitControls enableZoom={false} autoRotate />
                 <PerspectiveCamera position={[-1, 0, 1.8]} zoom={0.8} makeDefault />
