9bc6b7ebfc7c55021fca91e845626c9ac9c86fd8
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-09-20 12:39:37 -0500

    add function

--- src/components/services/computer/ComputerModelContainer.jsx
+++ src/components/services/computer/ComputerModelContainer.jsx
@@ -2,3 +2,9 @@ import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import { ComputerModel } from "./ComputerModel";
 import { OrbitControls, PerspectiveCamera, Stage } from "@react-three/drei";
+
+const ComputerModelContainer = () => {
+  return (
+   
+  );
+};
