ba804f184960e15037965ba5abc16f957f541f1c
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-09-21 15:17:32 -0500

    add group

--- src/components/services/console/ConsoleModel.jsx
+++ src/components/services/console/ConsoleModel.jsx
@@ -3,7 +3,9 @@ import { useGLTF } from '@react-three/drei'
 export function ConsoleModel(props){
     const { nodes, materials } = useGLTF('/consoleModel.glb')
     return(
-        
+        <group {...props} dispose={null}>
+            
+        </group>
     )
 
 }
