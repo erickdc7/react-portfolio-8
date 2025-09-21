f06f95f05327717fc435ebc90c77bc592ac02a3d
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-09-21 15:18:04 -0500

    add group

--- src/components/services/console/ConsoleModel.jsx
+++ src/components/services/console/ConsoleModel.jsx
@@ -5,6 +5,8 @@ export function ConsoleModel(props){
     return(
         <group {...props} dispose={null}>
              <group rotation={[-Math.PI / 2, 0, 0]} scale={1.112}>
+             <group rotation={[Math.PI / 2, 0, 0]} scale={0.01}>
+             </group>
              </group>
         </group>
     )
