476d4ba172611360e806a6da9ce9f02684b41cc9
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-09-21 15:22:05 -0500

    add mesh

--- src/components/services/mug/MugModel.jsx
+++ src/components/services/mug/MugModel.jsx
@@ -5,6 +5,7 @@ export function MugModel(props) {
     return (
         <group {...props} dispose={null}>
             <group rotation={[-Math.PI / 2, 0, 0]}>
+                <mesh geometry={nodes.Coffee_Coup_0.geometry} material={materials.Texture} position={[0.4, -0.148, 0.221]} rotation={[0, 0, -0.852]} />
             </group>
         </group>
     )
