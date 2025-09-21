a36d8db497448bac789d9d0696d0b5d46cde8af9
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-09-21 15:21:53 -0500

    add group

--- src/components/services/mug/MugModel.jsx
+++ src/components/services/mug/MugModel.jsx
@@ -4,7 +4,8 @@ export function MugModel(props) {
     const { nodes, materials } = useGLTF('/mugModel.glb')
     return (
         <group {...props} dispose={null}>
-
+            <group rotation={[-Math.PI / 2, 0, 0]}>
+            </group>
         </group>
     )
 }
