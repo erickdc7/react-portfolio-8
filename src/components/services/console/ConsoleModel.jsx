879a9b5416436d203a40c596a1aa4c3891272087
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-09-21 15:17:42 -0500

    add group

--- src/components/services/console/ConsoleModel.jsx
+++ src/components/services/console/ConsoleModel.jsx
@@ -4,7 +4,8 @@ export function ConsoleModel(props){
     const { nodes, materials } = useGLTF('/consoleModel.glb')
     return(
         <group {...props} dispose={null}>
-            
+             <group rotation={[-Math.PI / 2, 0, 0]} scale={1.112}>
+             </group>
         </group>
     )
 
