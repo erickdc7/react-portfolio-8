0f7cb95649189e988c09e33efc78fd9aeee611c5
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-09-21 15:21:25 -0500

    add const

--- src/components/services/mug/MugModel.jsx
+++ src/components/services/mug/MugModel.jsx
@@ -1,7 +1,7 @@
 import { useGLTF } from '@react-three/drei'
 
 export function MugModel(props) {
-    
+    const { nodes, materials } = useGLTF('/mugModel.glb')
 }
 
 useGLTF.preload('/mugModel.glb')
