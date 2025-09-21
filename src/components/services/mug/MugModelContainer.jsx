c1f1d43853a11b1767b8636859632e2225710cf9
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-09-21 15:25:21 -0500

    add code

--- src/components/services/mug/MugModelContainer.jsx
+++ src/components/services/mug/MugModelContainer.jsx
@@ -5,8 +5,16 @@ import { OrbitControls, PerspectiveCamera, Stage } from "@react-three/drei";
 
 const MugModelContainer = () => {
     return (
-        
-    )
-}
+        <Canvas>
+            <Suspense fallback="loading...">
+                <Stage environment="night" intensity={10}>
+                    <MugModel />
+                </Stage>
+                <OrbitControls enableZoom={false} autoRotate />
+                <PerspectiveCamera position={[0, -1, 2]} zoom={0.7} makeDefault />
+            </Suspense>
+        </Canvas>
+    );
+};
 
 export default MugModelContainer;
