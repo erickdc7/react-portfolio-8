5d70a8c0f6197a4dcfa3de80e4e8f3ccbb1b564a
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-09-21 15:16:59 -0500

    add const

--- src/components/services/console/ConsoleModel.jsx
+++ src/components/services/console/ConsoleModel.jsx
@@ -1,7 +1,8 @@
 import { useGLTF } from '@react-three/drei'
 
 export function ConsoleModel(props){
-    
+    const { nodes, materials } = useGLTF('/consoleModel.glb')
+
 }
 
 useGLTF.preload('/consoleModel.glb')
