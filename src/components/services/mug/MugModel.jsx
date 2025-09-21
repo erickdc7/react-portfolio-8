71e4f6beab593337bfc4349ec06d6550144cd3a0
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-09-21 15:21:34 -0500

    add return

--- src/components/services/mug/MugModel.jsx
+++ src/components/services/mug/MugModel.jsx
@@ -2,6 +2,9 @@ import { useGLTF } from '@react-three/drei'
 
 export function MugModel(props) {
     const { nodes, materials } = useGLTF('/mugModel.glb')
+    return(
+        
+    )
 }
 
 useGLTF.preload('/mugModel.glb')
