37239b143675b8db227e96c13f4b002a459c33e5
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-09-21 15:24:46 -0500

    add export

--- src/components/services/mug/MugModelContainer.jsx
+++ src/components/services/mug/MugModelContainer.jsx
@@ -2,3 +2,5 @@ import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import { MugModel } from "./MugModel";
 import { OrbitControls, PerspectiveCamera, Stage } from "@react-three/drei";
+
+export default MugModelContainer;
