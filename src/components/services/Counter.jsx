7537bbd45ffbc605e7579ee37de880549d9eac21
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-09-24 09:48:25 -0500

    add text

--- src/components/services/Counter.jsx
+++ src/components/services/Counter.jsx
@@ -1,7 +1,7 @@
 const Counter = ({ from, to, text }) => {
     return (
         <div className="counter">
-            <h1>29</h1>
+            <h1>{to}</h1>
             <p>Projects Completed</p>
         </div>
     )
