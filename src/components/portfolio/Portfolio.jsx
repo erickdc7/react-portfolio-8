81861ca3c33e8a869b34f36e0f1bc78cd82c8714
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-09-25 14:51:54 -0500

    add item

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,14 @@
 import "./portfolio.css"
 
 const items = [
-    
+    {
+        id: 1,
+        img: "/p1.jpg",
+        title: "Full Stack Blog Application",
+        desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure laboriosam tempore consectetur, atque maiores culpa quia, repellat id, dicta esse fugit neque voluptatem provident itaque voluptates minima. Repudiandae, provident hic.",
+        link: "/",
+    },
+   
 ];
 
 const Portfolio = () => {
