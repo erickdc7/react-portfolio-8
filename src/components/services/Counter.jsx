cfd68f1237423b91a65ed0f583d057aa52774a48
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-09-25 14:41:21 -0500

    add text

--- src/components/services/Counter.jsx
+++ src/components/services/Counter.jsx
@@ -20,7 +20,7 @@ const Counter = ({ from, to, text }) => {
 
     return (
         <div className="counter" ref={ref}>
-            <h1>{to}+</h1>
+            <h1>{count}+</h1>
             <p>{text}</p>
         </div>
     )
