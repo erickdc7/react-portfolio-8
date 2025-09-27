db484d03c4379be6caadd66eb7c0d6890c71efcd
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-09-27 17:46:28 -0500

    fix space

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -65,7 +65,11 @@ const Portfolio = () => {
 
     const { scrollYProgress } = useScroll({ target: ref })
 
-    const xTranslate = useTransform(scrollYProgress, [0, 1], [0, -window.innerWidth * items.length])
+    const xTranslate = useTransform(
+        scrollYProgress,
+        [0, 1],
+        [0, -window.innerWidth * items.length]
+    )
 
     return (
         <div className="portfolio" ref={ref}>
