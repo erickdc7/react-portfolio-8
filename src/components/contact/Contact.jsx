ff3aaa8b273dbbb6ef366190d0d01f7f41fccc24
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-09-29 10:58:49 -0500

    fix space

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,8 +3,13 @@ import "./contact.css"
 const Contact = () => {
     return (
         <div className="contact">
-            <div className="cSection"></div>
-            <div className="cSection"></div>
+            <div className="cSection">
+
+            </div>
+
+            <div className="cSection">
+
+            </div>
         </div>
     )
 }
