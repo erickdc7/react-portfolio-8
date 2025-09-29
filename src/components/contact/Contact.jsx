5e951f1ca98b98aebbd5eff99ffbfda1c2e841e0
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-09-29 11:12:00 -0500

    add text

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -24,7 +24,7 @@ const Contact = () => {
                         <textarea rows={10} placeholder="Write your message..."></textarea>
                     </div>
 
-                    <button></button>
+                    <button>Send</button>
                 </form>
             </div>
 
