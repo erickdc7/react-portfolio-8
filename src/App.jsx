58ceb3318e8e4036cbcb6ad9aedc9ca011aa8319
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-10-01 17:30:10 -0500

    add lazy

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import Hero from './components/hero/Hero'
 import Services from './components/services/Services'
 import Portfolio from './components/portfolio/Portfolio'
 import Contact from './components/contact/Contact'
+import { lazy } from 'react'
+
+
 
 const App = () => {
   return (
