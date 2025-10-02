01ae5e5825677f97183cf67ea7d24a914464c3fe
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-10-02 15:43:59 -0500

    add offset property

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const Contact = lazy(() => import("./components/contact/Contact"));
 const App = () => {
   return (
     <div className='container'>
-      <LazyLoad height={"100vh"}>
+      <LazyLoad height={"100vh"} offset={-100}>
         <section id="#home">
           <Hero />
         </section>
