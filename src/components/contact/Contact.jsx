939e2984caa74659568d3b61ab6874c569fbe682
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-10-03 12:20:21 -0500

    fix code

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,10 @@
-import { useRef } from "react";
-import "./contact.css"
-import { motion, useInView } from "motion/react"
+import "./contact.css";
+import emailjs from "@emailjs/browser";
+import { useRef, useState } from "react";
+import { motion, useInView } from "motion/react";
 import ContactSvg from "./ContactSvg";
 
-const listVariants = {
+const listVariant = {
     initial: {
         x: 100,
         opacity: 0,
@@ -14,59 +15,85 @@ const listVariants = {
         transition: {
             duration: 0.5,
             staggerChildren: 0.2,
-        }
-    }
+        },
+    },
 };
 
 const Contact = () => {
-    const isInView = useInView(ref, { margin: "-200px" })
+    const [success, setSuccess] = useState(false);
+    const [error, setError] = useState(false);
 
     const ref = useRef();
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(
+                import.meta.env.VITE_SERVICE_ID,
+                import.meta.env.VITE_TEMPLATE_ID,
+                form.current,
+                {
+                    publicKey: import.meta.env.VITE_PUBLIC_KEY,
+                }
+            )
+            .then(
+                () => {
+                    setSuccess(true);
+                    setError(false);
+                },
+                (error) => {
+                    console.log(error);
+                    setError(true);
+                    setSuccess(false);
+                }
+            );
+    };
+
+    const isInView = useInView(ref, { margin: "-200px" });
 
     return (
-        <div className="contact" ref={ref}>
+        <div className="contact" ref={ref} onSubmit={sendEmail}>
             <div className="cSection">
                 <motion.form
-                    variants={listVariants}
+                    ref={form}
+                    variants={listVariant}
                     animate={isInView ? "animate" : "initial"}
                 >
-                    <motion.h1
-                        className="cTitle"
-                        variants={listVariants}
-                    >
-                        Let`&apos;`s keep in touch
+                    <motion.h1 variants={listVariant} className="cTitle">
+                        Let&apos;s keep in touch
                     </motion.h1>
 
-                    <motion.div
-                        className="formItem"
-                        variants={listVariants}
-                    >
+                    <motion.div variants={listVariant} className="formItem">
                         <label>Name</label>
-                        <input type="text" placeholder="Jhon Doe" />
+                        <input type="text" name="user_username" placeholder="John Doe" />
                     </motion.div>
 
-                    <motion.div
-                        className="formItem"
-                        variants={listVariants}
-                    >
+                    <motion.div variants={listVariant} className="formItem">
                         <label>Email</label>
-                        <input type="email" placeholder="jhon@gmail.com" />
+                        <input
+                            type="email"
+                            name="user_email"
+                            placeholder="john@gmail.com"
+                        />
                     </motion.div>
 
-                    <motion.div
-                        className="formItem"
-                        variants={listVariants}
-                    >
+                    <motion.div variants={listVariant} className="formItem">
                         <label>Message</label>
-                        <textarea rows={10} placeholder="Write your message..."></textarea>
+                        <textarea
+                            rows={10}
+                            name="user_message"
+                            placeholder="Write your message..."
+                        ></textarea>
                     </motion.div>
 
-                    <motion.button
-                        className="formButton"
-                        variants={listVariants}
-                    >
+                    <motion.button variants={listVariant} className="formButton">
                         Send
                     </motion.button>
+
+                    {success && <span>Your message has been sent!</span>}
+                    {error && <span>Something went wrong!</span>}
                 </motion.form>
             </div>
 
@@ -74,7 +101,7 @@ const Contact = () => {
                 <ContactSvg />
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Contact
+export default Contact;
