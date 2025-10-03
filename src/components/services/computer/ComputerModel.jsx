2416f1bd36a858120aeb81d00e93a88d067bf04a
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-10-03 16:08:26 -0500

    fix code

--- src/components/services/computer/ComputerModel.jsx
+++ src/components/services/computer/ComputerModel.jsx
@@ -2,7 +2,6 @@ import { useGLTF } from "@react-three/drei";
 
 export function ComputerModel(props) {
     const { nodes, materials } = useGLTF("/computerModel.glb");
-
     return (
         <group {...props} dispose={null}>
             <group position={[0.121, 0.007, 0]}>
