4a1ee84fd9289a8f872addb3a0ea247aaa606a69
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-10-03 21:38:29 -0500

    fix code

--- src/components/services/mug/MugModelContainer.jsx
+++ src/components/services/mug/MugModelContainer.jsx
@@ -7,7 +7,8 @@ const MugModelContainer = () => {
     return (
         <Canvas>
             <Suspense fallback={null}>
-                 
+                <ambientLight intensity={1} />
+                
                 <OrbitControls enableZoom={false} autoRotate />
                 <PerspectiveCamera position={[0, -1, 2]} zoom={0.7} makeDefault />
             </Suspense>
