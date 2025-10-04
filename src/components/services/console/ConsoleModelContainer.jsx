a5aa22662f6e949e065d022376b3ed0044f276d3
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-10-04 14:44:54 -0500

    update position property

--- src/components/services/console/ConsoleModelContainer.jsx
+++ src/components/services/console/ConsoleModelContainer.jsx
@@ -10,7 +10,7 @@ const ConsoleModelContainer = () => {
                 <ambientLight intensity={0.5} />
                 <directionalLight position={[10, 10, 5]} intensity={1} />
                 <pointLight position={[-10, -10, -10]} intensity={0.5} />
-                <ConsoleModel scale={2.5} position={[0, -0.5, 0]} />
+                <ConsoleModel scale={2.5} position={[0, -0.6, 0]} />
                 <OrbitControls enableZoom={false} autoRotate />
                 <PerspectiveCamera position={[-1, 0, 1.8]} zoom={0.8} makeDefault />
             </Suspense>
