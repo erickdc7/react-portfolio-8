ceffd5d51fc301a606462cccfc66deb1ee0c6011
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-10-03 21:36:16 -0500

    fix code

--- src/components/services/computer/ComputerModelContainer.jsx
+++ src/components/services/computer/ComputerModelContainer.jsx
@@ -9,7 +9,7 @@ const ComputerModelContainer = () => {
             <Suspense fallback={null}>
                 <ambientLight intensity={0.5} />
                 <directionalLight position={[10, 10, 5]} intensity={1} />
-                
+                <pointLight position={[-10, -10, -10]} intensity={0.5} />
                 <OrbitControls enableZoom={false} autoRotate />
                 <PerspectiveCamera position={[-1, 0, 1.8]} zoom={0.8} makeDefault />
             </Suspense>
