4d86c997e71b2cfd5e34548ec2f21a4adbf788db
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2025-10-03 21:36:51 -0500

    fix code

--- src/components/services/console/ConsoleModelContainer.jsx
+++ src/components/services/console/ConsoleModelContainer.jsx
@@ -7,7 +7,8 @@ const ConsoleModelContainer = () => {
     return (
         <Canvas>
             <Suspense fallback={null}>
-                 
+                <ambientLight intensity={0.5} />
+                
                 <OrbitControls enableZoom={false} autoRotate />
                 <PerspectiveCamera position={[-1, 0, 1.8]} zoom={0.8} makeDefault />
             </Suspense>
